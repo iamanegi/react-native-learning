334ac564ca5d825b5bd3e3637d4305ddbdeba42f
aman <amannegi229@gmail.com>
2022-10-12 22:01:23 +0530

    added ui, handled text input and button click in goals app

--- Goals/App.js
+++ Goals/App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  function goalInputHandler(inputText) {
+    setEnteredGoalText(inputText);
+  }
+
+  function addGoalHandler() {
+    console.log(enteredGoalText);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Enter your task"
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Task" style={styles.button} onPress={addGoalHandler} />
+      </View>
+      <View style={styles.listContainer}>
+        <Text>List of goals:</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    padding: 16,
+    marginTop: 56,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "grey",
+    paddingBottom: 32,
+  },
+  listContainer: {
+    flex: 9,
+    marginTop: 16,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "grey",
+    borderRadius: 4,
+    width: "70%",
+    marginEnd: 8,
+    padding: 8,
+  },
+  button: {
+    alignContent: "center",
   },
 });
