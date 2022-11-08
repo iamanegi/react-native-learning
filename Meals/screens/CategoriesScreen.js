1af54ffe2a23b28a69c9ce26a7ea37562d058de2
aman <amannegi229@gmail.com>
2022-11-08 16:11:32 +0530

    organised items in a grid

---
+++ Meals/screens/CategoriesScreen.js
@@ -0,0 +1,18 @@
+import { FlatList } from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
+import { CATEGORIES } from "../data/dummy-data";
+
+export default function CategoriesScreen() {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+}
+
+function renderCategoryItem(itemData) {
+  return <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>;
+}
