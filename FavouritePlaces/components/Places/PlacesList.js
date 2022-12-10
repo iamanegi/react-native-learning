682cccd476a0734077719f7199e052f22f631feb
aman <amannegi229@gmail.com>
2022-12-10 19:11:55 +0530

    added Image picker

---
+++ FavouritePlaces/components/Places/PlacesList.js
@@ -0,0 +1,33 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { Colors } from "../../constants/colors";
+import PlaceItem from "./PlaceItem";
+
+export default function PlacesList({ places }) {
+  if (!places || places.lenght === 0) {
+    return (
+      <View style={styles.fallbackContainer}>
+        <Text style={styles.fallbackText}>No places added yet!</Text>
+      </View>
+    );
+  }
+
+  return (
+    <FlatList
+      data={places}
+      keyExtractor={(place) => place.id}
+      renderItem={({ item }) => <PlaceItem place={item} />}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  fallbackContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  fallbackText: {
+    fontSize: 16,
+    color: Colors.primary200,
+  },
+});
---
+++ FavouritePlaces/components/UI/ImagePicker.js
@@ -0,0 +1,68 @@
+import { launchCameraAsync, PermissionStatus, useCameraPermissions } from "expo-image-picker";
+import { useState } from "react";
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
+import { Colors } from "../../constants/colors";
+import OutlinedButton from "./OutlinedButtton";
+
+export default function ImagePicker() {
+  const [cameraPermissionInfo, requestPermission] = useCameraPermissions();
+  const [pickedImage, setPickedImage] = useState();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInfo.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+    if (cameraPermissionInfo.status === PermissionStatus.DENIED) {
+      Alert.alert("Permission Not Granted!", "You have not granted the persmission to the app.");
+      return false;
+    }
+    return true;
+  }
+
+  async function captureImageHandler() {
+    const hasPersmisison = await verifyPermissions();
+
+    if (!hasPersmisison) {
+      return;
+    }
+
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    setPickedImage(image.uri);
+  }
+
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image source={{ uri: pickedImage }} style={styles.image} />;
+  }
+
+  return (
+    <View>
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <OutlinedButton icon="camera" onPress={captureImageHandler}>
+        Capture image
+      </OutlinedButton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
